6514e256446298a2a55e1864bab8ff94c96e3b54
gerardind <dorian.gerardin@etu.umontpellier.fr>
2020-03-05 11:28:29 +0100

    fin ex 1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,18 @@
+let div_autocp = document.getElementById("autocompletion");
+
 function afficheVilles(tableau) {
-	// à compléter
+	if (div_autocp.children.length > 0) {
+		videVilles();
+	}
+	for (var i = 0; i < tableau.length; i++) {
+		let ville = document.createElement("p");
+		ville.innerHTML = tableau[i];
+		div_autocp.appendChild(ville);
+	}
+}
+
+function videVilles() {
+	for (var i = div_autocp.children.length - 1; i >= 0; i--) {
+		div_autocp.removeChild(div_autocp.children[i]);
+	}
 }
