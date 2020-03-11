06209e7883fd4680f48feed178b966b4937a6a11
gerardind <dorian.gerardin@etu.umontpellier.fr>
2020-03-11 13:55:02 +0100

    fin ex2

--- src/php/requeteVille.php
+++ src/php/requeteVille.php
@@ -5,14 +5,17 @@ require_once('Model.php');
 // A COMPLETER
 
 // récupération du contenu du champ, passé en get
-// ...
+$ville = $_GET["ville"];
 
 // lancement de la requête SQL avec selectByName et
 // récupération du résultat de la requête SQL
-// ...
+$tab = Model::selectByName($ville);
+
+echo json_encode($tab);
 
 // délai fictif
 // sleep(1);
 
 // affichage en format JSON du résultat précédent
 // ...
+
